1d59f80db97592fbff7ac9bb09330f9c6dd4ee87
Zsivics Mihael <mihael.zsivics@sap.com>
2017-08-28 15:04:51 +0200

    password removed from user

--- src/com/vehiclemanager/entity/User.java
+++ src/com/vehiclemanager/entity/User.java
@@ -6,17 +6,15 @@ public class User {
 	private String firstName;
 	private String lastName;
 	private String email;
-	private String password;
 
 	public User() {
 
 	}
 
-	public User(String firstName, String lastName, String email, String password) {
+	public User(String firstName, String lastName, String email) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
-		this.password = password;
 	}
 
 	public String getId() {
@@ -49,13 +47,5 @@ public class User {
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 }
